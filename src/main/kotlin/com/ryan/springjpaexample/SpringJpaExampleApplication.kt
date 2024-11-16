24884486e0aebab3fb9a5ca9972ee759b3771b20
Ryan-Sin <ryansin937@gmail.com>
2024-11-16 18:17:25 +0900

    first commit

---
+++ src/main/kotlin/com/ryan/springjpaexample/SpringJpaExampleApplication.kt
@@ -0,0 +1,11 @@
+package com.ryan.springjpaexample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringJpaExampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringJpaExampleApplication>(*args)
+}
---
+++ src/test/kotlin/com/ryan/springjpaexample/SpringJpaExampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ryan.springjpaexample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringJpaExampleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
