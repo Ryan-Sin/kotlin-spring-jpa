076c15a6f4542f1ea80ab4c65f0f76fed2d06c45
Ryan-Sin <ryansin937@gmail.com>
2024-12-10 14:35:40 +0900

    fix: 순수 jpa create update 메거니즘과 spring data jpa 메거니즘 다른 부분 확인

--- src/main/kotlin/com/ryan/kotlinspirngjpa/SpringJpaExampleApplication.kt
+++ src/main/kotlin/com/ryan/kotlinspirngjpa/SpringJpaExampleApplication.kt
@@ -2,10 +2,8 @@ package com.ryan.kotlinspirngjpa
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
-@EnableJpaAuditing
 class SpringJpaExampleApplication
 
 fun main(args: Array<String>) {
