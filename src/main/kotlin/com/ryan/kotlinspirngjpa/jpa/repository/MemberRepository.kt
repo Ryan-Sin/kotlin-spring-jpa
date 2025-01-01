f67773890b0f372364323c96c84b3ac07c46b41d
Ryan-Sin <ryansin937@gmail.com>
2025-01-02 04:55:40 +0900

    fix: 테스트 환경으로 변경

---
+++ src/main/kotlin/com/ryan/kotlinspirngjpa/jpa/repository/MemberRepository.kt
@@ -0,0 +1,43 @@
+package com.ryan.kotlinspirngjpa.jpa.repository
+
+import com.ryan.kotlinspirngjpa.jpa.entity.MemberEntity
+import jakarta.persistence.EntityManager
+
+class MemberRepository(private val em: EntityManager) {
+
+    fun save(member: MemberEntity): MemberEntity {
+        this.em.transaction.begin()
+        try {
+            this.em.persist(member)
+            this.em.transaction.commit()
+        } catch (e: Exception) {
+            this.em.transaction.rollback()
+        }
+
+        return member
+    }
+
+    fun findById(id: Long): MemberEntity? {
+        return this.em.find(MemberEntity::class.java, id)
+    }
+
+    fun update(member: MemberEntity) {
+        this.em.transaction.begin()
+        try {
+            this.em.persist(member)
+            this.em.transaction.commit()
+        } catch (e: Exception) {
+            this.em.transaction.rollback()
+        }
+    }
+
+    fun deleteById(member: MemberEntity) {
+        this.em.transaction.begin()
+        try {
+            this.em.remove(member)
+            this.em.transaction.commit()
+        } catch (e: Exception) {
+            this.em.transaction.rollback()
+        }
+    }
+}
